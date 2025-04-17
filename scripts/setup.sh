3bee422a3a5faaf4f747ea1dded696c898c118ac
AuditClay <42394776+AuditClay@users.noreply.github.com>
2025-04-17 07:54:29 -0500

    added nginx restart to setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+echo '#################################'
+echo 'updating lab files'
+echo '#################################'
+
+cd /home/student/labFiles
+git pull
+
+echo '#################################'
+echo 'Removing old kubernetes pods'
+echo '#################################'
+
 microk8s kubectl get pods
 for pod in $(microk8s kubectl get pods | awk '/Terminating/ {print $1}' ); do 
   microk8s kubectl delete pod $pod --force 
 done
 microk8s kubectl get pods
 
-cd /home/student/labFiles
-git pull
+echo '#################################'
+echo 'Restarting Nginx web server'
+echo '#################################'
+sudo systemctl daemon-reload
+sudo systemctl restart nginx.service
