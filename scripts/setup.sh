7f91db818d9323c1a28e572324dbf82a7dab8ea9
AuditClay <42394776+AuditClay@users.noreply.github.com>
2025-07-03 10:43:34 -0500

    setup.sh handle k8s services

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,25 @@ echo '#################################'
 
 cd /home/student/labFiles
 git pull
+echo '#################################'
+echo 'Removing old kubernetes pods'
+echo '#################################'
+microk8s kubectl get pods
+for pod in $(microk8s kubectl get pods | awk '/Terminating/ {print $1}' ); do 
+  microk8s kubectl delete pod $pod --force 
+done
+
+echo '#################################'
+echo 'Rescaling k8s deployments'
+echo '#################################'
+microk8s kubectl get deployments
+for dep in $(microk8s kubectl get deployments | awk '/\/1/ {print $1}' ); do 
+  microk8s kubectl scale deployment $dep --replicas=1
+done
+
+echo 'Sleeping 10 seconds'
+sleep 10
+microk8s kubectl get deployments
 
 echo '#################################'
 echo 'Removing old kubernetes pods'
@@ -13,6 +32,7 @@ microk8s kubectl get pods
 for pod in $(microk8s kubectl get pods | awk '/Terminating/ {print $1}' ); do 
   microk8s kubectl delete pod $pod --force 
 done
+
 echo '#################################'
 echo 'Current kubernetes pods'
 echo '#################################'
