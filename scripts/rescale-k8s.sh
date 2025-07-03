01ce973b92257b8ba3cb205eadf133d0bfab0f6d
AuditClay <42394776+AuditClay@users.noreply.github.com>
2025-07-03 10:42:41 -0500

    rescale script added nginx commands

--- scripts/rescale-k8s.sh
+++ scripts/rescale-k8s.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+echo '#################################'
+echo 'Removing old kubernetes pods'
+echo '#################################'
+microk8s kubectl get pods
+for pod in $(microk8s kubectl get pods | awk '/Terminating/ {print $1}' ); do 
+  microk8s kubectl delete pod $pod --force 
+done
+
 echo '#################################'
 echo 'Rescaling k8s deployments'
 echo '#################################'
@@ -7,6 +16,7 @@ microk8s kubectl get deployments
 for dep in $(microk8s kubectl get deployments | awk '/\/1/ {print $1}' ); do 
   microk8s kubectl scale deployment $dep --replicas=1
 done
+
 echo 'Sleeping 10 seconds'
 sleep 10
 microk8s kubectl get deployments
