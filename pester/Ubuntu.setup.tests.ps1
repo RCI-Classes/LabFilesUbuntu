a80adbb421e892a37234d37cac3e4682398d8219
AuditClay <42394776+AuditClay@users.noreply.github.com>
2025-07-03 10:08:05 -0500

    removed "hostonly" from pester

--- pester/Ubuntu.setup.tests.ps1
+++ pester/Ubuntu.setup.tests.ps1
@@ -34,9 +34,9 @@ Describe 'Lab Setup tests for 507Ubuntu VM' {
   
     #Check basic network setup to ensure local and internet connectivity
     Context 'Network connectivity' {
-        It 'Ping 507Win10 - HostOnly' {
+        It 'Ping 507Win10' {
             $arpRes = (sudo arping -c 1 10.50.7.101 | awk '/transmitted/ { print $4 }')
-            $arpRes | Should -BeExactly 1 -Because 'Ensure that first network adapter is set to HostOnly'
+            $arpRes | Should -BeExactly 1 -Because 'Ensure that first network adapter is configured'
         }
 
         # No outbound ping on Azure NAT gateway
